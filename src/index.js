dfbf5aac4d4b5e68ed4357b8fbd537307bb3ec2b
allen <eepson123@gmail.com>
2022-02-11 23:27:05 +0800

    feat:新增畫面 結構

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import '@/scss/all.scss';
 
 $(document).ready(function () {
   console.log('Webpack loaded!');
+$('body').css('background','#F6F8FA')
+
+
+
+
+
+
+  // $('.table').click(function(){
+  //   console.log('123123');
+  // })
+
+
 });
 
-// $('body').css('background','#f20')
